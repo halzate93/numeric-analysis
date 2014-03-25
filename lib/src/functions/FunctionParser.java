16611e27d6929c32993fe98eafa593778788c5e3
Christian Barrero <christian_barrero@hotmail.com>
2014-03-24 23:44:52 -0500

    añadido lib jeval, método para evaluar strings
    Método en la clase FunctionParser.

--- lib/src/functions/FunctionParser.java
+++ lib/src/functions/FunctionParser.java
@@ -1,7 +1,12 @@
 package functions;
 
 public class FunctionParser {
+	
+	private Evaluator evalutor;
+	
 	public static Function parse(String function){
-		return new Function();
+		evalutor = new Evaluator();
+		String resultInString = evalutor.evaluate(function);
+		return resultInString;
 	}
 }

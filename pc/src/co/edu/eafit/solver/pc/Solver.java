18e451daabb5dd1cda65c8fe71682e4c6171f83b
Héctor Camilo Alzate Ramírez <halzate93@gmail.com>
2014-04-01 02:06:46 -0500

    Finished basic console application implementation and deleted bugged Android implementation.

---
+++ pc/src/co/edu/eafit/solver/pc/Solver.java
@@ -0,0 +1,50 @@
+package co.edu.eafit.solver.pc;
+
+import java.util.Scanner;
+
+import co.edu.eafit.solver.pc.interpreters.*;
+
+import methods.EMethod;
+
+public class Solver {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int method = 0;
+		if(args.length == 0){
+			method = -1;
+			while(method == -1){
+				System.out.println("Which method do you want to use?");
+				for(EMethod m : EMethod.values()){
+					System.out.println(m.ordinal() + ": " + m.toString());
+				}
+				method = sc.nextInt();
+			}
+		}else{
+			method = Integer.parseInt(args[0]);
+		}
+		EMethod methodEnum = EMethod.values()[method];
+		try {
+			run(sc, methodEnum);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static void run(Scanner sc, EMethod method) throws Exception{
+		Interpreter i = null;
+		switch (method) {
+		case IncrementalSearch:
+			i = new IncrementalSearchInterpreter(sc, EMethod.IncrementalSearch);
+			break;
+
+		default:
+			break;
+		}
+		if(i != null) i.run();
+	}
+
+}
---
+++ pc/src/co/edu/eafit/solver/pc/interpreters/IncrementalSearchInterpreter.java
@@ -0,0 +1,38 @@
+package co.edu.eafit.solver.pc.interpreters;
+
+import java.util.Scanner;
+
+import org.json.JSONObject;
+
+import methods.EMethod;
+import methods.EParameter;
+
+public class IncrementalSearchInterpreter extends Interpreter {
+
+	public IncrementalSearchInterpreter(Scanner inputScanner, EMethod method)
+			throws Exception {
+		super(inputScanner, method);
+	}
+
+	@Override
+	public JSONObject configure(Scanner inputScanner) {
+		System.out.println("Input f(x):");
+		inputScanner.nextLine(); //Dismiss one line.
+		String f = inputScanner.nextLine();
+		System.out.println("Input dx:");
+		float dx = inputScanner.nextFloat();
+		System.out.println("Input x0:");
+		float x0 = inputScanner.nextFloat();
+		System.out.println("Input n:");
+		int n = inputScanner.nextInt();
+		
+		JSONObject parameters = new JSONObject();
+		parameters.put(EParameter.Function.toString(), f);
+		parameters.put(EParameter.Dx.toString(), dx);
+		parameters.put(EParameter.X0.toString(), x0);
+		parameters.put(EParameter.N.toString(), n);
+		
+		return parameters;
+	}
+
+}

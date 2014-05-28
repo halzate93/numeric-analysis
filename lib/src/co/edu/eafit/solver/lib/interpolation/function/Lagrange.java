4ed5df8a3b396b445bd6fa7a0947b09ab9b78334
halzate93 <halzate93@gmail.com>
2014-05-28 03:27:13 -0500

    Implemented Lagrange interpolation method, fixes #80

---
+++ lib/src/co/edu/eafit/solver/lib/interpolation/function/Lagrange.java
@@ -0,0 +1,30 @@
+package co.edu.eafit.solver.lib.interpolation.function;
+
+
+public class Lagrange extends FunctionInterpolation {
+	
+	@Override
+	protected double evaluate() {
+		double acum = 0;
+		for (int i = 0; i < points.length; i++) {
+			acum += points[i][1] * p[i];
+		}
+		return acum;
+	}
+
+	@Override
+	protected double[] calculatePolinomy() throws Exception {
+		double[] l = new double[points.length];
+		for (int i = 0; i < l.length; i++) {
+			l[i] = 1;
+			for (int p = 0; p < points.length; p++) {
+				if(p != i){
+					l[i] *= (x - points[p][0])/(points[i][0] - points[p][0]);
+				}
+			}
+		}
+		return l;
+	}
+	
+
+}
---
+++ lib/test/co/edu/eafit/solver/lib/test/interpolation/function/LagrangeTest.java
@@ -0,0 +1,52 @@
+package co.edu.eafit.solver.lib.test.interpolation.function;
+
+import static org.junit.Assert.*;
+
+import org.json.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
+
+import co.edu.eafit.solver.lib.interpolation.EInterpolationParameter;
+import co.edu.eafit.solver.lib.interpolation.function.Lagrange;
+import co.edu.eafit.solver.lib.systemsolver.MatrixUtility;
+
+public class LagrangeTest {
+
+	private static double[][] points = {{0.4, 6.219231}, {0.6, 7.271719},
+										{0.8, 8.824608}, {1, 11.145858}};
+	private static double x = 0.63;
+	
+	private static double[] p = {-0.039313, 0.904188, 0.159563, -0.024438};
+	private static double y = 7.466204;
+	
+	private Lagrange lg;
+	
+	@Before
+	public void setUp() throws Exception {
+		JSONObject parameters = new JSONObject();
+		parameters.put(EInterpolationParameter.Points.toString(),
+				MatrixUtility.matrix2Json(points));
+		parameters.put(EInterpolationParameter.X.toString(), x);
+		
+		lg = new Lagrange();
+		lg.setParameters(parameters);
+	}
+
+	@Test
+	public void testRun() throws Exception {
+		lg.interpolate();
+	}
+	
+	@Test
+	public void testP() throws Exception{
+		lg.interpolate();
+		assertTrue(MatrixUtility.compareVector(p, lg.getP(), 0.000001));
+	}
+	
+	@Test
+	public void testSolution() throws Exception{
+		lg.interpolate();
+		assertEquals(y, lg.getY(), 0.000001);
+	}
+
+}
